e6ab81142d4c9df1c0c1d4d5e0726480329b39b0
배소원 <engel5526@naver.com>
2022-03-23 23:59:14 +0900

    add get post to brand

--- backend/src/main/java/com/ssafy/cstars/service/implementation/BrandServiceImpl.java
+++ backend/src/main/java/com/ssafy/cstars/service/implementation/BrandServiceImpl.java
@@ -2,30 +2,48 @@ package com.ssafy.cstars.service.implementation;
 
 import com.ssafy.cstars.api.request.BrandDeleteReq;
 import com.ssafy.cstars.api.request.BrandPostReq;
+
 import com.ssafy.cstars.api.response.BrandRes;
 import com.ssafy.cstars.domain.entity.Brand;
+import com.ssafy.cstars.domain.entity.BrandAdmin;
 import com.ssafy.cstars.domain.repository.BradnRepository;
+import com.ssafy.cstars.domain.repository.BrandAdminRepository;
 import com.ssafy.cstars.service.BrandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.*;
 
 @Service("BrandService")
 public class BrandServiceImpl implements BrandService {
 
     @Autowired
     BradnRepository brandRepository;
+    @Autowired
+    BrandAdminRepository brandAdminRepository;
 
 
     @Override
-    public Page<Brand> GetBrandList(Pageable pageable) {
+    public List<BrandRes> GetBrandList() {
 
-        Page<Brand> brands = brandRepository.findAll(pageable);
-        if(brands.isEmpty()) return null;
-        return brands;
+        List<Brand> brands = brandRepository.findAll();
+
+        List<BrandRes> res = new ArrayList<>();
+
+        brands.forEach( brand -> {
+
+            List<BrandAdmin> byName = brandAdminRepository.findByName(brand.getName());
+            List<String> adminEmail = new ArrayList<>();
+
+            byName.forEach( name -> {
+                adminEmail.add(name.getEmail());
+            });
+            res.add(BrandRes.of(brand, adminEmail));
+        });
+
+        return res;
     }
 
     @Override
@@ -38,6 +56,9 @@ public class BrandServiceImpl implements BrandService {
 
         Brand brand = Brand.builder()
                 .name(brandInfo.getName())
+                .endDate(brandInfo.getEndDate())
+                .wallet(brandInfo.getWallet())
+                .imageUrl(brandInfo.getImageUrl())
                 .build();
 
         if(brand == null) return 500;
@@ -58,5 +79,6 @@ public class BrandServiceImpl implements BrandService {
 
         return 200;
     }
+
 
 }
