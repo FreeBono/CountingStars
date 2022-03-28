27c8ca897fb690d675007e7342af7a2e0b7d50f8
최호준 <matt9504@naver.com>
2022-03-28 10:31:46 +0900

    merge for jenkins

--- backend/src/main/java/com/ssafy/cstars/api/controller/IpfsController.java
+++ backend/src/main/java/com/ssafy/cstars/api/controller/IpfsController.java
@@ -39,50 +39,6 @@ public class IpfsController {
                                                           @RequestPart(value = "image")  @ApiParam(value = "IPFS 이미지", required = true) MultipartFile imageFile)  throws IOException, ClassNotFoundException{
         // 배포할 privateKey or 지갑 주소도 받아야 함
 
-<<<<<<< HEAD
-        System.out.println("테스트 잘 넘어오나???");
-//        System.out.println(getServerIp());
-//        System.out.println(ipfsInfo);
-
-        String[] ipAddress = { "/ip4/0.0.0.0/tcp/5001", "/ip4/172.17.0.1/tcp/5001",
-                "/ip4/127.0.0.1/tcp/5001", "/ip4/172.18.0.1/tcp/5001", "/ip4/172.26.14.69/tcp/5001" };
-
-        IPFS ipfs = null;
-        for(int i = 0; i < ipAddress.length; i++) {
-            try {
-                IPFS ipfs1 = new IPFS(ipAddress[i]);
-                System.out.println(ipAddress[i] + " ---------------------- 연결성공 -------------------------------");
-            } catch(Exception e) {}
-        }
-
-        if(ipfs != null) {
-            NamedStreamable.FileWrapper image = new NamedStreamable.FileWrapper(multipartFileToFile(imageFile));
-            MerkleNode addResult = ipfs.add(image).get(0);
-            String pinId = addResult.hash.toBase58();
-            System.out.println(pinId);
-
-            ipfsInfo.setImageUrl(pinId);
-
-            ObjectMapper mapper = new ObjectMapper();
-            String jsonStr = mapper.writeValueAsString(ipfsInfo);
-
-            NamedStreamable.ByteArrayWrapper file = new NamedStreamable.ByteArrayWrapper(ipfsInfo.getSerialNumber() + ".json", convertObjectToBytes(jsonStr));
-            addResult = ipfs.add(file).get(0);
-
-            // get
-
-            byte[] ipfsCat = ipfs.cat(addResult.hash);
-            Object ipfsObject = convertBytesToObject(ipfsCat);
-
-            System.out.println(ipfsObject);
-
-//        IPFSCluster ipfsCluster = new IPFSCluster("127.0.0.1", 9094);
-//        ipfsCluster.pins.add(pinId);
-        }
-
-//
-        return ResponseEntity.status(200).body(BaseResponseBody.of(200, "addResult.hash.toBase58()"));
-=======
         IpfsRes ipfsRes = ipfsService.addFileToIpfs(ipfsInfo, imageFile);
 
         if(ipfsRes != null)
@@ -94,7 +50,6 @@ public class IpfsController {
 //            return ResponseEntity.status(200).body(BaseResponseBody.of(200, result[1]));
 //        else
 //            return ResponseEntity.status(500).body(BaseResponseBody.of(500, "Connection Fail"));
->>>>>>> 82508e29f1da102f1df54f1ae8438610a657f723
     }
 
     @GetMapping("/{cid}")
@@ -112,51 +67,4 @@ public class IpfsController {
         else
             return ResponseEntity.status(500).body(null);
     }
-<<<<<<< HEAD
-
-    /*
-     * byte[]에서 객체로 변환
-     */
-    public static Object convertBytesToObject(byte[] bytes)
-            throws IOException, ClassNotFoundException {
-        InputStream is = new ByteArrayInputStream(bytes);
-        try (ObjectInputStream ois = new ObjectInputStream(is)) {
-            return ois.readObject();
-        }
-    }
-
-    /**
-     * multipartFile을 File로 변환한다.
-     */
-    public static File multipartFileToFile(MultipartFile file) throws IOException {
-
-        File convFile = new File(file.getOriginalFilename());
-        convFile.createNewFile();
-        FileOutputStream fos = new FileOutputStream(convFile);
-        fos.write(file.getBytes());
-        fos.close();
-
-        return convFile;
-
-    }
-
-    private String getServerIp() {
-        InetAddress local = null;
-        try {
-            local = InetAddress.getLocalHost();
-        }
-        catch ( UnknownHostException e ) {
-            e.printStackTrace();
-        }
-        if( local == null ) {
-            return "";
-        }
-        else {
-            String ip = local.getHostAddress();
-            return ip;
-        }
-
-    }
-=======
->>>>>>> 82508e29f1da102f1df54f1ae8438610a657f723
 }
